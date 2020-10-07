f8175e4ed9d05d36c7da0b2453d4e7949f57b074
Adam <thomasadam83@hotmail.com>
2020-10-06 23:07:10 -0400

    Added ino and Tinkercad files

---
+++ arduino_laser.ino
@@ -0,0 +1,1 @@
+int laserPin = 2;void setup() {  	pinMode(laserPin, OUTPUT);	}void loop() {  	digitalWrite(laserPin, HIGH);	delay(1000);		digitalWrite(laserPin, LOW);	delay(1000); 	}
